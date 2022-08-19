274c0bd36aa205e7f434b94b3638f2f99d17e705
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2022-08-19 14:44:37 +0500

    added install wrapper script
    Former-commit-id: fd1a80cfa50ed67da54fce3f869444d3ce17e89f

---
+++ Install_Raptor_Artifact/gen_exe/ubuntu_os_dep.sh
@@ -0,0 +1,37 @@
+sudo apt-get update
+sudo apt install -y bzip2 \
+  tclsh \
+  tcl-dev \
+  build-essential \
+  google-perftools \
+  libgoogle-perftools-dev \
+  uuid-dev \
+  lcov \
+  valgrind \
+  xorg \
+  qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtdeclarative5-dev \
+  xvfb \
+  tcllib \
+  bison \
+  flex \
+  libreadline-dev \
+  gawk \
+  libffi-dev \
+  git \
+  graphviz \
+  xdot \
+  pkg-config \
+  python3 \
+  libboost-system-dev \
+  libboost-python-dev \
+  libboost-filesystem-dev \
+  zlib1g-dev \
+  ninja-build \
+  libgtk-3-0 \
+  libgtk-3-dev \
+  libcairo2 \
+  libcairo2-dev \
+  libx11-6 \
+  libx11-dev \
+  libboost-all-dev \
+  libssl-dev
