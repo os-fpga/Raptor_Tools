671886ce3036401fc889c2f7d396110080fe692c
Raza-jafari-RS <raza.jafari@rapidsilicon.com>
2022-12-19 14:59:30 +0500

    reduced the unnecessary code

--- gatelevel_readers/read_edif/src/cells_sep.hpp
+++ gatelevel_readers/read_edif/src/cells_sep.hpp
@@ -1,112 +1,111 @@
 #ifndef CELL_SEP
 #define CELL_SEP
+#include "../src/edif_parsing.hpp"
 #include <assert.h>
+#include <iostream>
+#include <map>
 #include <stdio.h>
 #include <string.h>
-#include <iostream>
-#include <vector>
 #include <tuple>
-#include <map>
 #include <unordered_map>
-#include "../src/edif_parsing.hpp"
-class cells_sep
-{
+#include <vector>
+class cells_sep {
 public:
-    std::tuple<
-        /*Port name original  */ std::string,
-        /*Port name renamed   */ std::string,
-        /*Port direction      */ std::string,
-        /*Port size           */ std::string>
-        ports_tuple;
+  // std::tuple<
+  //     /*Port name original  */ std::string,
+  //     /*Port name renamed   */ std::string,
+  //     /*Port direction      */ std::string,
+  //     /*Port size           */ std::string>
+  //     ports_tuple;
 
+  // std::vector<std::tuple<
+  //     /*Port name original  */ std::string,
+  //     /*Port name renamed   */ std::string,
+  //     /*Port direction      */ std::string,
+  //     /*Port size           */ std::string>>
+  //     ports_vector;
+
+  // std::tuple<
+  //     /*instance name original  */ std::string,
+  //     /*Instance name renamed   */ std::string,
+  //     /*Instance cell reference  */ std::string,
+  //     /*Instance property lut    */ std::string,
+  //     /*Instance property width   */ std::string,
+  //     /* check is lut */              bool>
+  //     instance_tuple;
+
+  // std::vector<std::tuple<
+  //     /*instance name original  */ std::string,
+  //     /*Instance name renamed   */ std::string,
+  //     /*Instance cell reference  */ std::string,
+  //     /*Instance property lut    */ std::string,
+  //     /*Instance property width   */ std::string,
+  //     /* check is lut */                  bool>>
+  //     instance_vector;
+
+  // std::tuple<
+  //     //*net name original  */ std::string,
+  //     /*net port reference  */ std::string,
+  //     /*net pin number   */ std::string,
+  //     /* net instance ref  */ std::string,
+  //     /*net name original  */ std::string>
+  //     net_tuple;
+
+  std::vector<std::tuple<
+      //*net name original  */ std::string,
+      /*net port reference  */ std::string,
+      /*net pin number   */ std::string,
+      /* net instance ref  */ std::string,
+      /*net name original  */ std::string>>
+      net_vector;
+
+  // std::unordered_map<std::string, std::vector<std::tuple<
+  //                           //*net name original  */ std::string,
+  //                           /*net port reference  */ std::string,
+  //                           /*net pin number   */ std::string,
+  //                           /* net instance ref  */ std::string,
+  //                             /*net name original  */ std::string>>>
+  //     net_map;
+
+  struct cells {
+    std::string cell_name_orig;
+    std::string cell_name_renamed;
+    std::vector<std::tuple<
+        /*instance name original  */ std::string,
+        /*Instance name renamed   */ std::string,
+        /*Instance cell reference  */ std::string,
+        /*Instance property lut    */ std::string,
+        /*Instance property width   */ std::string,
+        /* check is lut */ bool>>
+        instance_vector;
     std::vector<std::tuple<
         /*Port name original  */ std::string,
         /*Port name renamed   */ std::string,
         /*Port direction      */ std::string,
         /*Port size           */ std::string>>
         ports_vector;
-
- 
-    std::tuple<
-        /*instance name original  */ std::string,
-        /*Instance name renamed   */ std::string,
-        /*Instance cell reference  */ std::string,
-        /*Instance property lut    */ std::string,
-        /*Instance property width   */ std::string,
-        /* check is lut */              bool>
-        instance_tuple;
-
-    std::vector<std::tuple<
-        /*instance name original  */ std::string,
-        /*Instance name renamed   */ std::string,
-        /*Instance cell reference  */ std::string,
-        /*Instance property lut    */ std::string,
-        /*Instance property width   */ std::string,
-        /* check is lut */                  bool>>
-        instance_vector;
-
-    std::tuple<
-        //*net name original  */ std::string,
-        /*net port reference  */ std::string,
-        /*net pin number   */ std::string,
-        /* net instance ref  */ std::string,
-        /*net name original  */ std::string>
-        net_tuple;
-
-    std::vector<std::tuple<
-        //*net name original  */ std::string,
-        /*net port reference  */ std::string,
-        /*net pin number   */ std::string,
-        /* net instance ref  */ std::string,
-        /*net name original  */ std::string>>
-        net_vector;
-
     std::unordered_map<std::string, std::vector<std::tuple<
-                              //*net name original  */ std::string,
-                              /*net port reference  */ std::string,
-                              /*net pin number   */ std::string,
-                              /* net instance ref  */ std::string,
-                                /*net name original  */ std::string>>>
+                                        /*net port reference  */ std::string,
+                                        /*net pin number   */ std::string,
+                                        /* net instance ref  */ std::string,
+                                        /*net name original  */ std::string>>>
         net_map;
+  };
 
-    struct cells
-    {
-        std::string cell_name_orig;
-        std::string cell_name_renamed;
-        std::vector<std::tuple<
-            /*instance name original  */ std::string,
-            /*Instance name renamed   */ std::string,
-            /*Instance cell reference  */ std::string,
-            /*Instance property lut    */ std::string,
-            /*Instance property width   */ std::string,
-            /* check is lut */ bool>>
-            instance_vector;
-        std::vector<std::tuple<
-            /*Port name original  */ std::string,
-            /*Port name renamed   */ std::string,
-            /*Port direction      */ std::string,
-            /*Port size           */ std::string>>
-            ports_vector;
-        std::unordered_map<std::string, std::vector<std::tuple<
-                                  //*net name original  */ std::string,
-                                  /*net port reference  */ std::string,
-                                  /*net pin number   */ std::string,
-                                  /* net instance ref  */ std::string,
-                                  /*net name original  */ std::string>>>
-            net_map;
-    };
-
-    std::string top_module;
-    void ports(struct SNode *head, std::string port_name,struct cells *cell_);
-    void instances(struct SNode *head, std::string instance_name,struct cells *cell_);
-    void get_nets(struct SNode *head, std::string net_name, std::string net_name_remaned,struct cells *cell_);
-    void nets(struct SNode *head, std::string net_name, struct cells *cell_);
-    void get_cell_data(struct SNode *head, std::string cell_name, struct cells *cell_);
+  std::string top_module;
+  void ports(struct SNode *head, std::string port_name, struct cells *cell_);
+  void instances(struct SNode *head, std::string instance_name,
+                 struct cells *cell_);
+  void get_nets(struct SNode *head, std::string net_name,
+                std::string net_name_remaned, struct cells *cell_);
+  void nets(struct SNode *head, std::string net_name, struct cells *cell_);
+  void get_cell_data(struct SNode *head, std::string cell_name,
+                     struct cells *cell_);
 
 public:
-   // bool string_compare(const std::string &f_name, const std::string &ext);
-    std::map<std::string, struct cells> cells_map;
-    void iterate(struct SNode *head);
+  // bool string_compare(const std::string &f_name, const std::string &ext);
+  std::map<std::string, struct cells> cells_map;
+  void iterate(struct SNode *head);
 };
 
 #endif
