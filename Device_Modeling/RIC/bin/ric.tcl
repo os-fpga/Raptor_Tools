b1576815ab9e7377974714c8c970cf22e7cd9886
Manadher Kharroubi <manadher.kharroubi@rapidsilicon.com>
2023-02-07 09:48:34 -0800

    Implemented Parameter syntactical checks , object template starter and interfaces until define_block

--- Device_Modeling/RIC/bin/ric.tcl
+++ Device_Modeling/RIC/bin/ric.tcl
@@ -14,32 +14,330 @@ set model  [lindex $argv 0]
 
 puts "RIC: Processing model: $model..."
 
+set ::enum_scope [ dict create ]
+
+set ::block_scope [ dict create ]
+
+set ::optionpat {
+    ^[\-]([a-z]+)?$
+}
+
+set ::version_pat {
+    ^\d+(\.\d+){2}$
+}
+
+set ::simple_id_pat {
+    ^[a-zA-Z_]([a-zA-Z0-9\[\]_-]+)?$
+}
+
+set ::hier_id_pat {
+    ^[a-zA-Z_]([a-zA-Z0-9\[\]_-]+)?(\.[a-zA-Z_]([a-zA-Z0-9\[\]_-]+)?)?$
+}
+
+set ::par_name_ports [ dict create  \
+    -name { 1 simple_id hier_id } \
+    -ports { -1 list } \
+    -force { 0 Nill } \
+]
+
+set ::par_name_value_List [ dict create  \
+    -name { 1 simple_id hier_id } \
+    -values { -1 list } \
+    -force { 0 Nill } \
+]
+
+set ::ric_schema_version 0.0
+set ::device_name None
+set ::device_version 0.0
+
+proc dm_type value {
+    if { [regexp -expanded $::simple_id_pat $value ] } {
+        return simple_id
+    } elseif { [regexp -expanded $::hier_id_pat $value ] } {
+        return hier_id
+    } elseif { [regexp -expanded $::version_pat $value ] } {
+        return version
+    }  elseif { [regexp -expanded $::optionpat $value ] } {
+        return option
+    }   elseif {[string is integer -strict $value]} {
+        return  integer
+    } elseif {[string is double -strict $value]} {
+        return  double
+    } elseif {[string is boolean -strict $value]} {
+        return  boolean
+    } elseif {[string is xdigit -strict $value]} {
+        return  xdigit
+    } elseif {[string is alpha -strict $value]} {
+        return  alpha
+    } elseif {[string is list -strict $value]} {
+        return  list
+    } else {
+        return __unknown_type__
+    }
+}
+
+proc verify_make_params { params actuals } {
+    set idx 0
+    set dictRes [dict create valid 1 ]
+    while { $idx < [llength $actuals ] } {
+        set op [ lindex $actuals $idx ]
+        set tp [ dm_type $op ]
+        if { $tp != "option" } {
+            puts "Too much values for last option at this value \"$op\""
+            puts "Got $tp while expecting option"
+            dict set dictRes valid 0
+            return $dictRes
+        }
+        if { ! [dict exists $params $op] } {
+            puts "Unknown option : $op"
+            dict set dictRes valid 0
+            return $dictRes
+        }
+        set valDesc [ dict get $params $op ]
+        if { "list" != [ dm_type $valDesc ] } {
+            puts "Not well formed option dictionary"
+            dict set dictRes valid 0
+            return $dictRes
+        }
+        set numValues [ lindex $valDesc 0 ]
+        incr idx
+        if { ! $numValues } {
+            dict set dictRes $op 1
+            continue
+        }
+        set values {  }
+        if { $numValues > 0 } {
+            while { $idx < [llength $actuals ] && $numValues } {
+                set typeCheck 0
+                set i 1
+                while { $i < [llength $valDesc ] } {
+                    set elem [ lindex $actuals $idx ]
+                    set elType [ dm_type $elem ]
+                    set expectedType [ lindex $valDesc $i ]
+                    set typeCheck [ expr { $typeCheck || [ expr { $elType ==  $expectedType } ] }  ]
+                    lappend values $elem
+                    if { $typeCheck } {
+                        break
+                    }
+                    incr i
+                }
+                if { ! $typeCheck } {
+                    puts "The option $op takes values of type $expectedType , $elem is of type $elType"
+                    dict set dictRes valid 0
+                    return $dictRes
+                }
+                incr idx
+                incr numValues -1
+            }
+            if { $numValues } {
+                puts "The option $op takes $numValues values"
+                dict set dictRes valid 0
+                return $dictRes
+            }
+            if { [dict exists $dictRes $op] } {
+                puts "The option $op is specified twice"
+                dict set dictRes valid 0
+                return $dictRes
+            }
+            dict set dictRes $op $values
+        }
+        if { $numValues == -1 } {
+            set cnt 0
+            set elem [ lindex $actuals $idx ]
+            set elType [ dm_type [ lindex $actuals $idx ] ]
+            set expectedType [ lindex $valDesc 1 ]
+
+            set values { }
+            while { $idx < [llength $actuals ] && "option" != $elType  } {
+                if { $expectedType != $elType } {
+                    puts "The option $op takes values of type [ lindex $valDesc 1 ]"
+                    dict set dictRes valid 0
+                    return $dictRes
+                }
+                incr idx
+                incr cnt
+                lappend values $elem
+                set elem [ lindex $actuals $idx ]
+                set elType [ dm_type [ lindex $actuals $idx ] ]
+            }
+            if { ! $cnt } {
+                puts "The option $op needs at least one argument"
+                dict set dictRes valid 0
+                return $dictRes
+            }
+            if { [dict exists $dictRes $op] } {
+                puts "The option $op is specified twice"
+                dict set dictRes valid 0
+                return $dictRes
+            }
+            dict set dictRes $op $values
+        }
+
+    }
+    return $dictRes
+}
+
+#===================================================================
+#                           RIC Commands
+#===================================================================
+
 proc ric_schema_version { args } {
-    # -version <version> string // the version of the Device Modeling Schema (e.g 1.0.2)
-    set options {-version}
+    set tp [ dm_type $args ]
+    set dictRes [dict create valid 1 ]
+    if { $tp == "list"  || $tp == "double" || $tp == "version" } {
+        set ::ric_schema_version $args
+        return $dictRes
+    }
+    puts "Invalid ric_schema_version : $args"
+    dict set dictRes valid 0
+    return $dictRes
 }
 
 proc device_name  { args } {
-    # -name <name>   string // Defines the Device name (e.g GEMINI)
-    set options {-name}
+    set tp [ dm_type $args ]
+    set dictRes [dict create valid 1 ]
+    if { $tp == "simple_id" } {
+        set ::device_name $args
+        return $dictRes
+    }
+    puts "Invalid device_name : $args"
+    dict set dictRes valid 0
+    return $dictRes
 }
 
 proc device_version  { args } {
-    # -version <version>  string // Defines the version of the Device (e.g 1.0.2)
-    set options {-version}
+    set tp [ dm_type $args ]
+    set dictRes [dict create valid 1 ]
+    if { $tp == "list"  || $tp == "double" || $tp == "version" } {
+        set ::device_version $args
+        return $dictRes
+    }
+    puts "Invalid device_version : $args"
+    dict set dictRes valid 0
+    return $dictRes
 }
 
 proc define_enum_type { args } {
     # -name <enumerate>
     # -values { {<name>, value }, …, {<name>, value }, {"default", value } }
-    set options {-name -values}
+    # -force Nill   // Overrides older definition
+    # example : puts [ define_enum_type -name myAttrType -values { { one 0x7 } { two 0x2 } { three 0x3} { default 0x9 } } -force ]
+
+    set paramDict $::par_name_value_List
+    set actual [ verify_make_params $paramDict $args ]
+    set valuesDict [dict create ]
+    set max -1
+    set min 1000000
+    dict set actual valid 0
+    foreach elem [ lindex [ dict get $actual "-values" ] 0 ] {
+        set tp  [ dm_type $elem ]
+        if { $tp != "list" } {
+            puts "An enum element should be a name vlue pair"
+            return $actual
+        }
+        set l [ llength $elem]
+        if { 2 > $l || 3 < $l } {
+            if {$l } {
+                puts "An enum element should be a name vlue pair at enum \"[lindex $elem 0]\""
+            } else {
+                puts "An enum element should be a name vlue pair , empty enum spec$elem in [ dict get $actual "-name" ]"
+            }
+            return $actual
+        }
+        set name [lindex $elem 0]
+        set value [lindex $elem 1]
+        set ntp [ dm_type $name ]
+        set vtp [ dm_type $value ]
+        if { "simple_id" != $ntp || "integer" != $vtp || $value < 0 } {
+            puts "Mal formed attribute element { $elem }"
+            return $actual
+        }
+        if {  [dict exists $valuesDict $name ] } {
+            puts "$epeated attribute element { $elem }, ignoring"
+            continue
+        }
+        dict set valuesDict $name $value
+        # allows for default value to be un-named to allow forcing an distinct setting when needed
+        if { $value < $min } {
+            set min $value
+        }
+        if { $value > $max } {
+            set max $value
+        }
+    }
+    set width 1
+    set m 1
+    while { ($m << $width) <= $max } {
+        incr width
+    }
+    dict set actual "-width" $width
+    dict set actual "-values" $valuesDict
+
+    if { ! [dict exists $::enum_scope [ dict get $actual "-name" ] ] || [dict exists $actual "-force" ]} {
+        dict set actual valid 1
+        dict set ::enum_scope [ dict get $actual "-name" ] $actual
+        return 1
+    }
+    return 0
 }
 
 proc define_block  { args } {
-    # -name <type>           string // The name of the currently defined attribute
-    # -ports  <list_of_port_objects> [ {port} ] // Set of ports
-    # -force                         Nill    // Rewrite existing block if  previously any named same
-    set options {-name -ports -force }
+    # -name <type>                      string // The name of the currently defined block
+    # -ports  <list_of_port_objects>..  [ {DIR port , port} ] // Set of ports
+    # Example : define_block -ports { in a b c } { out aa bb cc } { in ss } -name kkk
+
+    set paramDict $::par_name_ports
+    set actual  [ verify_make_params $paramDict $args ]
+    # Fast access to keys, values not used for now, may get width or usage type (clk)
+    set inputs  [ dict create ]
+    set outputs [ dict create ]
+    dict set actual valid 0
+    if { [ dict exists $actual "-ports"] } {
+        foreach port_set [ dict get $actual "-ports" ]  {
+            if { "in" == [ lindex $port_set 0 ] } {
+                set idx 1
+                while {$idx < [llength $port_set ] } {
+                    set in [ lindex $port_set $idx ]
+                    if { "simple_id" != [ dm_type $in ]} {
+                        puts "Not a suitable port name $in"
+                        return 0
+                    }
+                    if { [dict exists $outputs $in ] || [dict exists $inputs $in ]} {
+                        puts "Duplicated port name $out"
+                        return 0
+                    }
+                    dict set inputs $in 1
+                    incr idx
+                }
+            } elseif {"out" == [ lindex $port_set 0 ]  } {
+                set idx 1
+                while {$idx < [llength $port_set ] } {
+                    set out [ lindex $port_set $idx ]
+                    if { "simple_id" != [ dm_type $out ]} {
+                        puts "Not a suitable port name $out"
+                        return 0
+                    }
+                    if { [dict exists $outputs $out ] || [dict exists $inputs $out ]} {
+                        puts "Duplicated port name $out"
+                        return 0
+                    }
+                    dict set outputs $out 1
+                    incr idx
+                }
+            } else {
+                puts "Not a Valid specification of ports $port_set, should start with in or out"
+                return 0
+            }
+        }
+        dict set actual "-inputs" $inputs
+        dict set actual "-outputs" $outputs
+    }
+    if { ![dict exists $::block_scope [ dict get $actual "-name" ]  ]  || [dict exists $actual "-force" ]} {
+        dict set actual valid 1
+        dict set ::block_scope [ dict get $actual "-name" ] $actual
+        return 1
+    }
+    return 0
 }
 
 proc define_attr { args } {
