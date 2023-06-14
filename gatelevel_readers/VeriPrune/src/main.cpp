99d197ef896d1936600d9df6f52b1d0e735b34f1
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-06-14 11:06:01 +0500

    Changing default name for file created for VPR

--- gatelevel_readers/VeriPrune/src/main.cpp
+++ gatelevel_readers/VeriPrune/src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     // Get the file name to work on
     const char *file_name = (argc > 1) ? argv[1] : "../example/example.v" ;
 
-    const char *out_file_name = (argc > 2) ? argv[2] : "to_vpr.v" ;
+    const char *out_file_name = (argc > 2) ? argv[2] : "nl_to_vpr.v" ;
     const char *wrapper_file_name = (argc > 3) ? argv[3] : "wrapper.v" ;
     const char *file_base = ((argc > 3) ? argv[3] : "src") ;
 
