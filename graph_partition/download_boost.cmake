f8fea99eb3b65ec1fb23c0dd83685adddea14dda
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2023-08-17 19:22:09 +0500

    use cmake variables to set the output dir

--- graph_partition/download_boost.cmake
+++ graph_partition/download_boost.cmake
@@ -1,7 +1,8 @@
+set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/external_tools/boost)
 include(FetchContent)
 FetchContent_Populate(
   boost-src
   URL https://github.com/RapidSilicon/post_build_artifacts/releases/download/v0.1/boost_1_69_0_minimum.tar.bz2
   #URL_HASH MD5=a1332494397bf48332cb152abfefcec2
-  SOURCE_DIR mt-kahypar/external_tools/boost
+  SOURCE_DIR ${unzip_boost_dir}
 )
--- graph_partition/download_tbb_linux.cmake
+++ graph_partition/download_tbb_linux.cmake
@@ -1,7 +1,8 @@
+set(unzip_tbb_dir ${CMAKE_CURRENT_BINARY_DIR}/external_tools/tbb)
 include(FetchContent)
 FetchContent_Populate(
   tbb
   URL https://github.com/oneapi-src/oneTBB/releases/download/v2021.7.0/oneapi-tbb-2021.7.0-lin.tgz
   URL_HASH SHA256=3c2b3287c595e2bb833c025fcd271783963b7dfae8dc681440ea6afe5d550e6a
-  SOURCE_DIR mt-kahypar/external_tools/tbb
+  SOURCE_DIR ${unzip_tbb_dir}
 )
