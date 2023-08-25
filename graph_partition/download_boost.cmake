584178c937e14cebc94df938f34d8c8bf89c1076
Behnam <behnam.ghavami@rapidsilicon.com>
2023-08-25 16:18:50 -0700

    debug make for partition

--- graph_partition/download_boost.cmake
+++ graph_partition/download_boost.cmake
@@ -4,20 +4,46 @@ else()
     set(build_dir dbuild)
 endif()
 
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/Raptor_Tools)
-set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/${build_dir}/Raptor_Tools/graph_partition/mt-kahypar/external_tools/boost)
-elseif(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/../graph_partition)
-set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/${build_dir}/mt-kahypar/external_tools/boost)
-else()
-set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/opt)
+if($ENV{RAPTOR} STREQUAL "on")
+    set(RAPTOR on)
+elseif($ENV{BACKEND_RS} STREQUAL "on")
+    set(BACKEND_RS on)
 endif()
-include(FetchContent)
-FetchContent_Populate(
-  boost-src
-  URL https://github.com/RapidSilicon/post_build_artifacts/releases/download/v0.1/boost_1_69_0_minimum.tar.bz2
-  #URL_HASH MD5=a1332494397bf48332cb152abfefcec2
-  SOURCE_DIR ${unzip_boost_dir}
-  SUBBUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/${build_dir}/boost_download
-)
+
+if(RAPTOR)
+  if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/Raptor_Tools)
+  set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/${build_dir}/Raptor_Tools/graph_partition/mt-kahypar/external_tools/boost)
+  elseif(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/../graph_partition)
+  set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/${build_dir}/mt-kahypar/external_tools/boost)
+  else()
+  set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/opt)
+  endif()
+  include(FetchContent)
+  FetchContent_Populate(
+    boost-src
+    URL https://github.com/RapidSilicon/post_build_artifacts/releases/download/v0.1/boost_1_69_0_minimum.tar.bz2
+    #URL_HASH MD5=a1332494397bf48332cb152abfefcec2
+    SOURCE_DIR ${unzip_boost_dir}
+    SUBBUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/${build_dir}/boost_download
+  )
+elseif(BACKEND_RS)
+  message(STATUS "------------ BOOST BACKEND_RS")
+  if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/Raptor_Tools)
+  set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/Raptor_Tools/graph_partition/mt-kahypar/external_tools/boost)
+  elseif(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/../graph_partition)
+  set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/mt-kahypar/external_tools/boost)
+  else()
+  set(unzip_boost_dir ${CMAKE_CURRENT_BINARY_DIR}/opt)
+  endif()
+  include(FetchContent)
+  FetchContent_Populate(
+    boost-src
+    URL https://github.com/RapidSilicon/post_build_artifacts/releases/download/v0.1/boost_1_69_0_minimum.tar.bz2
+    #URL_HASH MD5=a1332494397bf48332cb152abfefcec2
+    SOURCE_DIR ${unzip_boost_dir}
+    SUBBUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/boost_download
+  )
+endif()
+
 #file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/boost-src-subbuild)
 #file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/boost-src-build)
