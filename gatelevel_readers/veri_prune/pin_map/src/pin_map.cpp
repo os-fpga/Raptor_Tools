4845b619885b0de8f1aff912ad8bd23da9804749
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-09-21 12:30:32 +0500

    Removing redundant prints

--- gatelevel_readers/veri_prune/pin_map/src/pin_map.cpp
+++ gatelevel_readers/veri_prune/pin_map/src/pin_map.cpp
@@ -40,11 +40,9 @@ void map_inputs (const json& data, std::string signalName, const std::string& di
             std::string actualSignal = portInfo["Actual"];
             if (portInfo.contains("FUNC")) {
                 std::string sigDir = portInfo["FUNC"];
-                std::cout << "ACtual name is  :: " << actualSignal << std::endl;
                 if (dir == "Input" && sigDir == "IN_DIR" && actualSignal == signalName) {
                     if (modName == "I_BUF" && portName == "I") {
                         std::string outPort = ports["O"]["Actual"];
-                        std::cout << "Out port is :: " << outPort << std::endl;
                         signalName = outPort;
                         if (conn.signal.empty()) {
                             conn.signal = actualSignal;
@@ -82,7 +80,6 @@ void map_inputs (const json& data, std::string signalName, const std::string& di
                 bool inputSignal;
                 if(sigDir == "IN_DIR" || sigDir == "IN_CLK" || sigDir == "IN_RESET") inputSignal = true;
                 if (dir == "Input" && inputSignal && actualSignal == signalName) {
-                    std::cout << "Found :: " << actualSignal << "   in mod ::  " << modName <<std::endl;
                     bool found = std::find(complexPrim.begin(), complexPrim.end(), modName) != complexPrim.end();
                     if (found) {
                         std::string tempSig;
@@ -95,10 +92,8 @@ void map_inputs (const json& data, std::string signalName, const std::string& di
                                 conn = Connection();
                                 conn.signal = tempSig;
                             }
-                            else{ std::cout << signalName << "  is alsooo in   " << modName << std::endl;}
                         }
                         conn.ports[portName] = actualSignal;
-                        //std::cout << "now sig is :: " << portName << std::endl;
                         conn.module = modName;
                         instConns[instanceName].push_back(conn);
                     }
@@ -115,10 +110,8 @@ void map_inputs (const json& data, std::string signalName, const std::string& di
                                 conn = Connection();
                                 conn.signal = tempSig;
                             }
-                            else{ std::cout << signalName << "  is alsooo in   " << modName << std::endl;}
                         }
                         conn.ports[portName] = actualSignal;
-                        //std::cout << "now sig is :: " << portName << std::endl;
                         conn.module = modName;
                         instConns[instanceName].push_back(conn);
                     }
@@ -160,7 +153,6 @@ int main() {
 
         for (const auto& pair : orig_ios) {
             if (pair.second == "Input") {
-                std::cout << pair.first << " is an input" << std::endl;
                 map_inputs(jsonData, pair.first, "Input");
             } else if (pair.second == "Output") {
                  map_inputs(jsonData, pair.first, "Output");
@@ -171,18 +163,6 @@ int main() {
     }
 
     std::cout << "////////////// vector //////////" << std::endl;
-    //for (const Connection& conn : connections) {
-    //    std::cout << "Signal: " << conn.signal << std::endl;
-    //    std::cout << "Ports:" << std::endl;
-    //    for (const auto& port : conn.ports) {
-    //        std::cout << "  " << port.first << ": " << port.second << std::endl;
-    //    }
-    //    std::cout << "Module: " << conn.module << std::endl;
-    //    std::cout << "Instance: " << conn.instance << std::endl;
-    //    std::cout << "Is Input: " << (conn.isInput ? "true" : "false") << std::endl;
-    //    std::cout << "--------------------------------------" << std::endl;
-    //}
-//
     // Example: Printing connections by instance
     for (const auto& entry : instConns) {
         const std::string& instance = entry.first;
