3f4fa45bf0057653b16a4e3142225a61aa53c0ec
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-10-18 12:08:53 +0500

    Removing pin_map and adding main.cpp

---
+++ gatelevel_readers/veri_prune/pin_map/src/main.cpp
@@ -0,0 +1,39 @@
+int main(int argc, char* argv[]) {
+  // Get the file name to work on
+    std::string intf_json = "interface.json";
+    std::string mod_ios = "mod_ios.json";
+    std::string user_sdc = "pc.pin";
+    std::string pin_table = "pin_table.csv";
+    std::string output_sdc = "output.sdc";
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if (arg == "-i" || arg == "--intf") {
+            if (i + 1 < argc) {
+                intf_json = argv[i + 1];
+                i++;
+            }
+        } else if (arg == "-io" || arg == "--ios") {
+            if (i + 1 < argc) {
+                mod_ios = argv[i + 1];
+                i++;
+            }
+        } else if (arg == "-u" || arg == "--user_sdc") {
+            if (i + 1 < argc) {
+                user_sdc = argv[i + 1];
+                i++;
+            }
+        } else if (arg == "-t" || arg == "--ptable") {
+            if (i + 1 < argc) {
+                pin_table = argv[i + 1];
+                i++;
+            }
+        } else if (arg == "-o" || arg == "--out") {
+            if (i + 1 < argc) {
+                output_sdc = argv[i + 1];
+                i++;
+            }
+        }
+    }
+}
