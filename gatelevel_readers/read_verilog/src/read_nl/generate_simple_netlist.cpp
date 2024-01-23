7a009a18894006769f43a01ba9170303bb121288
NadeemYaseen <nadeem.yaseen@rapidsilicon.com>
2024-01-23 11:50:42 +0500

    removed no longer used cpp files

--- gatelevel_readers/read_verilog/src/read_nl/generate_simple_netlist.cpp
+++
@@ -1,7 +0,0 @@
-#include "simple_netlist.h"
-
-int parse_verilog(const char *file_name, simple_netlist &n_l, const char *key_file, const char *top_mod)
-{
-    std::cout << "Verilog netlists not supported yet" << std::endl; 
-    return 0;
-}
