2ebcae46839a26127893ef6249bbcb7c018b953c
behzadmehmood <behzadmehmood82@gmail.com>
2024-02-07 17:42:07 +0500

    Adding makefile to run with yosys

--- gatelevel_readers/veri_prune/src/veri_prune/veri_prune.cpp
+++ gatelevel_readers/veri_prune/src/veri_prune/veri_prune.cpp
@@ -5,14 +5,25 @@
 #include <set>
 
 #include "gb_map.h"
+#include "kernel/celltypes.h"
+#include "backends/rtlil/rtlil_backend.h"
+#include "kernel/log.h"
+#include "kernel/register.h"
+#include "kernel/rtlil.h"
+#include "kernel/yosys.h"
+#include "kernel/mem.h"
+#include "kernel/ffinit.h"
+#include "kernel/ff.h"
 #include "veri_prune.h"
 #include <json.hpp>
 #include <ostream>
 using json = nlohmann::json;
-
+USING_YOSYS_NAMESPACE
+PRIVATE_NAMESPACE_BEGIN
 
 int prune_verilog(const char *file_name, gb_constructs &gb,
 				const std::string &device_name) {
 	std::cout << "Just started working on the tool" << std::endl;
 	return 0; // Status OK
 }
+PRIVATE_NAMESPACE_END
