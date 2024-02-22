bd9ff5cf0f058ae4b8dd8bf7e325d6d0bb3cba07
NadeemYaseen <nadeem.yaseen@rapidsilicon.com>
2024-02-23 02:51:01 +0500

    generate device map at run time

--- OpenLM/License_manager.cpp
+++ OpenLM/License_manager.cpp
@@ -6,18 +6,6 @@ using namespace std;
 const map<int, string> stringByStrategyId = {
 	{STRATEGY_DEFAULT, "DEFAULT"}, {STRATEGY_ETHERNET, "MAC"}, {STRATEGY_IP_ADDRESS, "IP"}, {STRATEGY_DISK, "Disk"}};
 
-const unordered_map<int, string> stringByEventType = {
-	{LICENSE_OK, "OK "},
-	{LICENSE_FILE_NOT_FOUND, "license file not found "},
-	{LICENSE_SERVER_NOT_FOUND, "license server can't be contacted "},
-	{ENVIRONMENT_VARIABLE_NOT_DEFINED, "environment variable not defined "},
-	{FILE_FORMAT_NOT_RECOGNIZED, "license file has invalid format (not .ini file) "},
-	{LICENSE_MALFORMED, "some mandatory field are missing, or data can't be fully read. "},
-	{PRODUCT_NOT_LICENSED, "this product is not licensed "},
-	{PRODUCT_EXPIRED, "license expired "},
-	{LICENSE_CORRUPTED, "license signature didn't match with current license "},
-	{IDENTIFIERS_MISMATCH, "Calculated identifier and the one provided in license didn't match"}};
-
 License_Manager::License_Manager(){}
 
 License_Manager::License_Manager(LicensedProductName licensedProductName)  : License_Manager() {
@@ -41,48 +29,5 @@ License_Manager::License_Manager(std::string licensedProductName) {
             throw LicenseFatalException();
         }
     }
-
-bool License_Manager::licenseCheckout(const string &productName) {
-    string featureName = productName;
-    LicenseLocation licLocation = {LICENSE_PATH};
-    //const char* env_var_value = std::getenv("LICENSE_LOCATION");
-    LicenseInfo licenseInfo;
-    size_t pc_id_sz = LCC_API_PC_IDENTIFIER_SIZE + 1;
-    char pc_identifier[LCC_API_PC_IDENTIFIER_SIZE + 1];
-
-    LCC_EVENT_TYPE result = acquire_license(nullptr, &licLocation, &licenseInfo);
-
-    if (result == LICENSE_OK) {
-        cout << "License for Raptor software OK" << endl;
-        CallerInformations callerInfo; 
-        auto it = DeviceMapping.find(productName);
-        if (it != DeviceMapping.end()) {
-            strcpy(callerInfo.feature_name, it->second);
-            } else {
-                    return false; 
-            }
-        result = acquire_license(&callerInfo, &licLocation, &licenseInfo);
-        if (result == LICENSE_OK) {
-            cout << productName.c_str() << "  is licensed" << endl;
-            if (!licenseInfo.linked_to_pc) {
-            cout << "No hardware signature in license file. This is a 'demo' license that works on every PC." << endl;
-            }
-            return true;
-        } else {
-            cout << productName.c_str() << "  is NOT licensed because " << stringByEventType.at(result) << endl;
-            return false;
-        }
-    } else {
-        cout << "License ERROR:" << endl;
-        cout << "    " << stringByEventType.at(result) << endl;
-        if (identify_pc(STRATEGY_DEFAULT, pc_identifier, &pc_id_sz, nullptr)) {
-            cout << "PC signature is:" << endl;
-            cout << "    " << pc_identifier << endl;
-        } else {
-            cout << "Errors in identify_pc" << endl;
-        }
-        return false;
-    }
-}
 License_Manager::~License_Manager(){}
 
