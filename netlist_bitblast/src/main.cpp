e6f06acdfbdf408e7db8b2994d56ef252cc986f7
alaindargelas <alain.dargelas@gmail.com>
2024-08-30 22:26:49 -0700

    Initial commit netlist bitblaster

---
+++ netlist_bitblast/src/main.cpp
@@ -0,0 +1,73 @@
+
+#if defined(_MSC_VER)
+#include <direct.h>
+#include <process.h>
+#else
+#include <sys/param.h>
+#include <unistd.h>
+#endif
+
+#include <string.h>
+#include <sys/stat.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+#include <Windows.h>
+#endif
+
+#include <Surelog/API/Surelog.h>
+#include <Surelog/CommandLine/CommandLineParser.h>
+#include <Surelog/ErrorReporting/ErrorContainer.h>
+#include <Surelog/SourceCompile/SymbolTable.h>
+
+#include <uhdm/uhdm.h>
+
+int main(int argc, const char** argv) {
+  
+  if (argc < 2) return 0;
+
+  // Read command line, compile a design, use -parse argument
+  int32_t code = 0;
+  SURELOG::SymbolTable *const symbolTable = new SURELOG::SymbolTable();
+  
+  SURELOG::ErrorContainer *const errors =
+      new SURELOG::ErrorContainer(symbolTable);
+  SURELOG::CommandLineParser *const clp =
+      new SURELOG::CommandLineParser(errors, symbolTable, false, false);
+  clp->noPython();
+  clp->setwritePpOutput(true);
+  clp->setParse(true);
+  clp->setCompile(true);
+  clp->setElaborate(true);  // Request Surelog instance tree elaboration
+  clp->setElabUhdm(false);   // Request (Not) UHDM Uniquification/Elaboration
+  bool success = clp->parseCommandLine(argc, argv);
+  errors->printMessages(clp->muteStdout());
+
+  vpiHandle vpi_design = nullptr;
+  SURELOG::scompiler *compiler = nullptr;
+  if (success && (!clp->help())) {
+    compiler = SURELOG::start_compiler(clp);
+    vpi_design = SURELOG::get_uhdm_design(compiler);
+    auto stats = errors->getErrorStats();
+    code = (!success) | stats.nbFatal | stats.nbSyntax | stats.nbError;
+  }
+
+  SURELOG::ErrorContainer::Stats stats = errors->getErrorStats();
+  errors->printStats(stats, false);
+
+  if (vpi_design == nullptr) return code;
+
+
+  // Do not delete these objects until you are done with UHDM
+  SURELOG::shutdown_compiler(compiler);
+  delete clp;
+  delete symbolTable;
+  delete errors;
+  
+  return 0;
+  
+}
