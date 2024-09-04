6db7f8866445385b05a7d7ac4b94fa5747499a0b
alaindargelas <alain.dargelas@gmail.com>
2024-09-03 17:45:00 -0700

    pretty printer wip

--- netlist_bitblast/src/NetlistPrettyPrinter.cpp
+++ netlist_bitblast/src/NetlistPrettyPrinter.cpp
@@ -1,4 +1,108 @@
+/*
+Copyright 2021-2024 Rapid Silicon
+
+GPL License
+
+Copyright (c) 2024 The Open-Source FPGA Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "NetlistPrettyPrinter.h"
 
+#include <uhdm/ElaboratorListener.h>
+#include <uhdm/ExprEval.h>
+#include <uhdm/NumUtils.h>
+#include <uhdm/clone_tree.h>
+#include <uhdm/containers.h>
+#include <uhdm/uhdm.h>
+#include <uhdm/uhdm_forward_decl.h>
+#include <uhdm/uhdm_types.h>
 
+using namespace UHDM;
 
+namespace BITBLAST {
+
+std::string NetlistPrettyPrinter::prettyPrint(const UHDM::any *handle) {
+  if (handle == nullptr) {
+    std::cout << "NULL HANDLE\n";
+    return "NULL HANDLE";
+  }
+  Serializer *s = handle->GetSerializer();
+  std::stringstream out;
+  prettyPrint(*s, handle, 0, out);
+  return out.str();
+}
+
+void NetlistPrettyPrinter::prettyPrint(UHDM::Serializer &s,
+                                       const UHDM::any *object, uint32_t indent,
+                                       std::ostream &out) {
+  if (object == nullptr) return;
+  UHDM::UHDM_OBJECT_TYPE type = object->UhdmType();
+  for (uint32_t i = 0; i < indent; i++) {
+    out << " ";
+  }
+  switch (type) {
+    case UHDM_OBJECT_TYPE::uhdmdesign: {
+      design *d = (design *)object;
+      m_design = d;
+      for (module_inst *top : *d->TopModules()) {
+        prettyPrint(s, top, 0, out);
+      }
+      break;
+    }
+    case UHDM_OBJECT_TYPE::uhdmmodule_inst: {
+      module_inst *c = (module_inst *)object;
+      out << "`timescale 1ns/1ps\n";
+      out << "module ";
+      std::string name = std::string(c->VpiName());
+      name = name.substr(name.find("@") + 1);
+      out << name;
+      out << "(";
+      int nbPorts = c->Ports()->size();
+      int index = 0;
+      for (port *p : *c->Ports()) {
+        prettyPrint(s, p, 0, out);
+        index++;
+        if (index < nbPorts) out << ", ";
+      }
+      out << ");";
+      out << "\n";
+      out << "endmodule";
+      out << "\n";
+      break;
+    }
+    case UHDM_OBJECT_TYPE::uhdmport: {
+      port *p = (port *)object;
+      uint32_t type = p->VpiDirection();
+      if (type == vpiInput) {
+        out << "input ";
+      } else if (type == vpiOutput) {
+        out << "output ";
+      } else if (type == vpiInput) {
+        out << "inout ";
+      } else {
+        out << "unknown:" << type;
+      }
+      out << p->VpiName();
+      break;
+    }
+    default: {
+      std::cout << "NOT HANDLED HANDLE\n";
+      break;
+    }
+  }
+}
+
+}  // namespace BITBLAST
--- netlist_bitblast/src/main.cpp
+++ netlist_bitblast/src/main.cpp
@@ -1,3 +1,23 @@
+/*
+Copyright 2021-2024 Rapid Silicon
+
+GPL License
+
+Copyright (c) 2024 The Open-Source FPGA Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #if defined(_MSC_VER)
 #include <direct.h>
@@ -23,18 +43,17 @@
 #include <Surelog/CommandLine/CommandLineParser.h>
 #include <Surelog/ErrorReporting/ErrorContainer.h>
 #include <Surelog/SourceCompile/SymbolTable.h>
-
 #include <uhdm/uhdm.h>
+
 #include "NetlistPrettyPrinter.h"
 
-int main(int argc, const char** argv) {
-  
+int main(int argc, const char **argv) {
   if (argc < 2) return 0;
 
   // Read command line, compile a design, use -parse argument
   int32_t code = 0;
   SURELOG::SymbolTable *const symbolTable = new SURELOG::SymbolTable();
-  
+
   SURELOG::ErrorContainer *const errors =
       new SURELOG::ErrorContainer(symbolTable);
   SURELOG::CommandLineParser *const clp =
@@ -43,7 +62,7 @@ int main(int argc, const char** argv) {
   clp->setwritePpOutput(true);
   clp->setParse(true);
   clp->setCompile(true);
-  clp->setElabUhdm(false);   // Request (Not) UHDM Uniquification/Elaboration
+  clp->setElabUhdm(false);  // Request (Not) UHDM Uniquification/Elaboration
   clp->setElaborate(true);  // Request Surelog instance tree elaboration
   bool success = clp->parseCommandLine(argc, argv);
   errors->printMessages(clp->muteStdout());
@@ -62,13 +81,19 @@ int main(int argc, const char** argv) {
 
   if (vpi_design == nullptr) return code;
 
+  BITBLAST::NetlistPrettyPrinter *printer =
+      new BITBLAST::NetlistPrettyPrinter();
+  std::string result =
+      printer->prettyPrint(UhdmDesignFromVpiHandle(vpi_design));
+  delete printer;
+
+  std::cout << "DESIGN:\n" << result << "\n";
 
   // Do not delete these objects until you are done with UHDM
   SURELOG::shutdown_compiler(compiler);
   delete clp;
   delete symbolTable;
   delete errors;
-  
+
   return 0;
-  
 }
